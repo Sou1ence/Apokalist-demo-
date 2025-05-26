ed17b94799fae1dd4ed558f8b2b8f882d58183fc
Sou1ence <feniuk.kostiantyn@gmail.com>
2025-05-26 23:55:27 +0200

    feat: final way

---
+++ apokalist_run.bat
@@ -0,0 +1,21 @@
+@echo off
+ECHO Starting Apokalist...
+
+cd /d "%~dp0"
+
+ECHO Starting the application...
+start /b java -jar target/Apokalist-0.0.1-SNAPSHOT.jar
+
+ECHO Waiting 5 seconds before opening browser...
+timeout /t 10 /nobreak
+
+ECHO Opening browser...
+start http://localhost:8081
+
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Failed to open browser
+    pause
+    exit /b 1
+)
+
+pause
